7b69e57a40c9274bf215921ec3df4f62e36942f6
qishenonly <1050026498@qq.com>
2023-05-07 19:39:26 +0800

    test: Merge func test

---
+++ merge_test.go
@@ -0,0 +1,100 @@
+package flydb
+
+import (
+	"github.com/qishenonly/flydb/utils"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+// 没有任何数据的情况下进行 merge
+func TestDB_Merge(t *testing.T) {
+	opts := DefaultOptions
+	dir, _ := os.MkdirTemp("", "flydb-merge-1")
+	opts.DirPath = dir
+	db, err := Open(opts)
+	//defer destroyDB(db)
+	//assert.Nil(t, err)
+	t.Log(err)
+	//assert.NotNil(t, db)
+
+	err = db.Merge()
+	t.Log(err)
+	//assert.Nil(t, err)
+}
+
+// 全部都是有效的数据
+func TestDB_Merge2(t *testing.T) {
+	opts := DefaultOptions
+	dir, _ := os.MkdirTemp("", "flydb-merge-2")
+	opts.DataFileSize = 32 * 1024 * 1024
+	opts.DirPath = dir
+	db, err := Open(opts)
+	defer destroyDB(db)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	for i := 0; i < 50000; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.RandomValue(1024))
+		assert.Nil(t, err)
+	}
+
+	err = db.Merge()
+	t.Log(err)
+	assert.Nil(t, err)
+
+	// 重启校验
+	err = db.Close()
+	assert.Nil(t, err)
+
+	db2, err := Open(opts)
+	defer func() {
+		_ = db2.Close()
+	}()
+	assert.Nil(t, err)
+	keys := db2.GetListKeys()
+	assert.Equal(t, 50000, len(keys))
+
+	for i := 0; i < 50000; i++ {
+		val, err := db2.Get(utils.GetTestKey(i))
+		assert.Nil(t, err)
+		assert.NotNil(t, val)
+	}
+
+}
+
+// 全部是无效的数据
+func TestDB_Merge3(t *testing.T) {
+	opts := DefaultOptions
+	dir, _ := os.MkdirTemp("", "flydb-merge-3")
+	opts.DataFileSize = 32 * 1024 * 1024
+	opts.DirPath = dir
+	db, err := Open(opts)
+	defer destroyDB(db)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	for i := 0; i < 50000; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.RandomValue(1024))
+		assert.Nil(t, err)
+	}
+	for i := 0; i < 50000; i++ {
+		err := db.Delete(utils.GetTestKey(i))
+		assert.Nil(t, err)
+	}
+
+	err = db.Merge()
+	assert.Nil(t, err)
+
+	// 重启校验
+	err = db.Close()
+	assert.Nil(t, err)
+
+	db2, err := Open(opts)
+	defer func() {
+		_ = db2.Close()
+	}()
+	assert.Nil(t, err)
+	keys := db2.GetListKeys()
+	assert.Equal(t, 0, len(keys))
+}
