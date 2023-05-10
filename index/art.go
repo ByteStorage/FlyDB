a6e1464ae9f58206ec1283a15f6f3a93461180bf
CrazyJius <jiustudio0505@163.com>
2023-05-10 11:04:20 +0800

    feat: Complete ART Interface

--- index/art.go
+++ index/art.go
@@ -21,12 +21,37 @@ func NewART() *AdaptiveRadixTree {
 	}
 }
 
-func (artree *AdaptiveRadixTree) Put(key []byte, pst *data.LogRecordPst) bool {}
+func (artree *AdaptiveRadixTree) Put(key []byte, pst *data.LogRecordPst) bool {
+	artree.lock.Lock()
+	defer artree.lock.Unlock()
+	artree.tree.Insert(key, pst)
+	return true
+}
 
-func (artree *AdaptiveRadixTree) Get(key []byte) *data.LogRecordPst {}
+func (artree *AdaptiveRadixTree) Get(key []byte) *data.LogRecordPst {
+	artree.lock.RLock()
+	defer artree.lock.RUnlock()
+	value, found := artree.tree.Search(key)
+	if !found {
+		return nil
+	}
+	return value.(*data.LogRecordPst)
+}
 
-func (artree *AdaptiveRadixTree) Delete(key []byte) bool {}
+func (artree *AdaptiveRadixTree) Delete(key []byte) bool {
+	artree.lock.Lock()
+	defer artree.lock.Unlock()
+	_, deleted := artree.tree.Delete(key)
+	return deleted
+}
 
-func (artree *AdaptiveRadixTree) Size() int {}
+func (artree *AdaptiveRadixTree) Size() int {
+	artree.lock.RLock()
+	defer artree.lock.RUnlock()
+	size := artree.tree.Size()
+	return size
+}
 
-func (artree *AdaptiveRadixTree) Iterator(reverse bool) Iterator {}
+func (artree *AdaptiveRadixTree) Iterator(reverse bool) Iterator {
+
+}
