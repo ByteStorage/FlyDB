ecf3546926bea04c841627560bfc53e4398e0655
CrazyJius <jiustudio0505@163.com>
2023-05-10 10:53:32 +0800

    feat: Init ART Interface

--- index/art.go
+++ index/art.go
@@ -2,6 +2,7 @@ package index
 
 import (
 	art "github.com/plar/go-adaptive-radix-tree"
+	"github.com/qishenonly/flydb/data"
 	"sync"
 )
 
@@ -11,3 +12,21 @@ type AdaptiveRadixTree struct {
 	tree art.Tree
 	lock *sync.RWMutex
 }
+
+// NewART Initializes the adaptive radix tree index
+func NewART() *AdaptiveRadixTree {
+	return &AdaptiveRadixTree{
+		tree: art.New(),
+		lock: new(sync.RWMutex),
+	}
+}
+
+func (artree *AdaptiveRadixTree) Put(key []byte, pst *data.LogRecordPst) bool {}
+
+func (artree *AdaptiveRadixTree) Get(key []byte) *data.LogRecordPst {}
+
+func (artree *AdaptiveRadixTree) Delete(key []byte) bool {}
+
+func (artree *AdaptiveRadixTree) Size() int {}
+
+func (artree *AdaptiveRadixTree) Iterator(reverse bool) Iterator {}
