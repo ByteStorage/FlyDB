64b5106d72f489fba0ceef6e7e14e3bfb1d9748e
qishenonly <1050026498@qq.com>
2023-05-27 16:43:29 +0800

    feat: complete simple logic

--- tcp/server.go
+++ tcp/server.go
@@ -1,6 +1,8 @@
 package tcp
 
 import (
+	"context"
+	"fmt"
 	"github.com/qishenonly/flydb/protocol/tcpIF"
 	"net"
 )
@@ -11,10 +13,29 @@ type Config struct {
 
 // ListenAndServeBySignal start tcp server by signal
 func ListenAndServeBySignal(cfg *Config, handler tcpIF.Handler) error {
+	listener, err := net.Listen("tcp", cfg.Address)
+	if err != nil {
+		return err
+	}
+	fmt.Println("tcp server start listen on: ", cfg.Address)
+
+	closeChan := make(chan struct{})
+	ListenAndServe(listener, handler, closeChan)
+
 	return nil
 }
 
 // ListenAndServe start tcp server
 func ListenAndServe(listener net.Listener, handler tcpIF.Handler, closeChan <-chan struct{}) {
-
+	ctx := context.Background()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			break
+		}
+		fmt.Println("accept new connection: ", conn.RemoteAddr().String())
+		go func() {
+			handler.Handle(ctx, conn)
+		}()
+	}
 }
