e26013bbf9cd9970144ba5100fc11ab0d5adcc4c
qishenonly <1050026498@qq.com>
2023-05-28 16:58:08 +0800

    comp: Complete TCP-ListenAndServe logic

--- tcp/server.go
+++ tcp/server.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/qishenonly/flydb/protocol/tcpIF"
 	"net"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
 type Config struct {
@@ -14,13 +17,33 @@ type Config struct {
 
 // ListenAndServeBySignal start tcp server by signal
 func ListenAndServeBySignal(cfg *Config, handler tcpIF.Handler) error {
+	closeChan := make(chan struct{})
+	// listen system-level signal
+	signalChan := make(chan os.Signal)
+	// syscall.SIGHUP: terminal closed
+	// syscall.SIGINT: ctrl + c
+	// syscall.SIGTERM: kill
+	// syscall.SIGQUIT: ctrl + \
+	signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// receive signal
+	// when receive a signal, send a signal to closeChan
+	// closeChan will close tcp server
+	go func() {
+		s := <-signalChan
+		switch s {
+		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			closeChan <- struct{}{}
+		}
+	}()
+
 	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
 	}
 	fmt.Println("tcp server start listen on: ", cfg.Address)
 
-	closeChan := make(chan struct{})
+	// start tcp server
 	ListenAndServe(listener, handler, closeChan)
 
 	return nil
