771daed23aea59b74123ae8fac208919137a1e76
sjc <1401189096@qq.com>
2023-05-31 14:47:55 +0800

    add grpc interface

---
+++ raft/grpc/grpc_interface.go
@@ -0,0 +1,15 @@
+package grpc
+
+type Message struct {
+}
+
+type Grpc interface {
+	SendMessage(addr string, msg Message) error
+}
+
+type Client struct {
+}
+
+func (c *Client) SendMessage(addr string, msg Message) error {
+	panic("implement me")
+}
