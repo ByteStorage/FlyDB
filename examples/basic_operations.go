51680a5408fd491323501f080d132c755f00d61d
bigboss2063 <bigboss2063@outlook.com>
2023-06-11 20:06:55 +0800

    fix: add windows temp file

--- examples/basic_operations.go
+++ examples/basic_operations.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/ByteStorage/flydb/config"
 	"github.com/ByteStorage/flydb/flydb"
+	"os"
 )
 
 func main() {
 	options := config.DefaultOptions
-	options.DirPath = "/tmp/flydb"
+	options.DirPath = os.TempDir() + "/flydb"
 	db, err := flydb.NewFlyDB(options)
 	if err != nil {
 		panic(err)
@@ -27,6 +28,16 @@ func main() {
 	fmt.Println("name value => ", string(val))
 
 	err = db.Delete([]byte("name"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.RemoveAll(options.DirPath)
 	if err != nil {
 		panic(err)
 	}
