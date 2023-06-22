14a14dac28a4dc18f372cc342707f773ea84b439
sjcsjc123 <1401189096@qq.com>
2023-06-22 15:37:28 +0800

    init

--- cluster/region/region.go
+++ cluster/region/region.go
@@ -5,3 +5,10 @@ type Region struct {
 	StartKey []byte
 	EndKey   []byte
 }
+
+type Manager interface {
+	// GetRegionByKey gets region and leader peer by region key from cluster.
+	GetRegionByKey(key []byte) (*Region, error)
+	// GetRegionByID gets region and leader peer by region id from cluster.
+	GetRegionByID(id uint64) (*Region, error)
+}
