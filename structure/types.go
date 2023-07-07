c71a9b2d9ae2f62d27dad9eab513bac208683095
qishenonly <1050026498@qq.com>
2023-07-07 20:19:44 +0800

    feat: Add two functions of conversion type

--- structure/types.go
+++ structure/types.go
@@ -47,21 +47,126 @@ func interfaceToBytes(value interface{}) ([]byte, error, string) {
 	}
 }
 
+//func byteToInterface(value []byte, dataType string) (interface{}, error) {
+//	switch dataType {
+//	case "string":
+//		return string(value), nil
+//	case "int":
+//		return strconv.Atoi(string(value))
+//	case "int64":
+//		return strconv.ParseInt(string(value), 10, 64)
+//	case "float64":
+//		return strconv.ParseFloat(string(value), 64)
+//	case "bool":
+//		return strconv.ParseBool(string(value))
+//	case "[]byte":
+//		return value, nil
+//	default:
+//		return nil, errors.New("unsupported type")
+//	}
+//}
+
 func byteToInterface(value []byte, dataType string) (interface{}, error) {
 	switch dataType {
 	case "string":
 		return string(value), nil
 	case "int":
-		return strconv.Atoi(string(value))
+		intValue, err := strconv.Atoi(string(value))
+		if err != nil {
+			return nil, errors.New("cannot convert to int: " + err.Error())
+		}
+		return intValue, nil
 	case "int64":
-		return strconv.ParseInt(string(value), 10, 64)
+		int64Value, err := strconv.ParseInt(string(value), 10, 64)
+		if err != nil {
+			return nil, errors.New("cannot convert to int64: " + err.Error())
+		}
+		return int64Value, nil
 	case "float64":
-		return strconv.ParseFloat(string(value), 64)
+		float64Value, err := strconv.ParseFloat(string(value), 64)
+		if err != nil {
+			return nil, errors.New("cannot convert to float64: " + err.Error())
+		}
+		return float64Value, nil
 	case "bool":
-		return strconv.ParseBool(string(value))
+		boolValue, err := strconv.ParseBool(string(value))
+		if err != nil {
+			return nil, errors.New("cannot convert to bool: " + err.Error())
+		}
+		return boolValue, nil
 	case "[]byte":
 		return value, nil
 	default:
 		return nil, errors.New("unsupported type")
+	}
+}
+
+// convertToInt converts an interface to an int
+func convertToInt(Value interface{}) (int, error) {
+	switch value := Value.(type) {
+	case int:
+		return value, nil
+	case int32:
+		return int(value), nil
+	case int64:
+		return int(value), nil
+	case float32:
+		return int(value), nil
+	case float64:
+		return int(value), nil
+	case uint:
+		return int(value), nil
+	case uint8:
+		return int(value), nil
+	case uint16:
+		return int(value), nil
+	case uint32:
+		return int(value), nil
+	case uint64:
+		return int(value), nil
+	case string:
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			return 0, err
+		}
+		return intValue, nil
+	case []byte:
+		strValue := string(value)
+		intValue, err := strconv.Atoi(strValue)
+		if err != nil {
+			return 0, err
+		}
+		return intValue, nil
+	default:
+		return 0, errors.New("unsupported type")
+	}
+}
+
+// convertToFloat converts an interface to a float64
+func convertToFloat(Value interface{}) (float64, error) {
+	switch value := Value.(type) {
+	case int:
+		return float64(value), nil
+	case uint:
+		return float64(value), nil
+	case float32:
+		return float64(value), nil
+	case float64:
+		return value, nil
+	case string:
+		floatValue, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return 0, err
+		}
+		return floatValue, nil
+	case []byte:
+		strValue := string(value)
+		floatValue, err := strconv.ParseFloat(strValue, 64)
+		if err != nil {
+			return 0, err
+		}
+		return floatValue, nil
+	default:
+		return 0, errors.New("unsupported type")
 	}
 }
