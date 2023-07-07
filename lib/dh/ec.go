7f3b58a9e71c6cbd3afc76b7d56d66853fda39f2
WangChenGuang <1289418375@qq.com>
2023-07-07 11:28:09 +0800

    add implement function AssignData(#81)

--- lib/dh/ec.go
+++ lib/dh/ec.go
@@ -1,7 +1,6 @@
 package dh
 
 import (
-	"errors"
 	"github.com/ByteStorage/FlyDB/lib/raft"
 	"github.com/klauspost/reedsolomon"
 )
@@ -19,20 +18,5 @@ func NewEcEncoder(dataNum int, parityNum int) (EcEncoder, error) {
 }
 
 func (ec *EcEncoder) AssignData(data []byte, slaveAddrList []cluster.Slave) (map[string][]byte, error) {
-	dataShards, err := ec.Split(data)
-	if err != nil {
-		return nil, err
-	}
-
-	// Check if the number of shards is more than the number of slaves
-	if len(dataShards) > len(slaveAddrList) {
-		return nil, errors.New("number of data shards is more than the number of slaves")
-	}
-
-	assignment := make(map[string][]byte)
-	for i, shard := range dataShards {
-		assignment[slaveAddrList[i].Addr] = shard
-	}
-
-	return assignment, nil
+	panic("implement me")
 }
