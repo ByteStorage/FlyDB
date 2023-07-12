05db0ed24b449641bad4275c3d7bb679be849d3f
WangChenGuang <1289418375@qq.com>
2023-07-12 16:49:38 +0800

    add implement some functions in string.go such as stringMSet stringMGet stringGetSet stringExist.go(#188)

--- cmd/client/root.go
+++ cmd/client/root.go
@@ -1,8 +1,11 @@
 package client
 
-import "github.com/desertbit/grumble"
+import (
+	"github.com/desertbit/grumble"
+)
 
 func register(app *grumble.App) {
+
 	app.AddCommand(&grumble.Command{
 		Name: "put",
 		Help: "put data",
@@ -31,4 +34,40 @@ func register(app *grumble.App) {
 		},
 	})
 
+	app.AddCommand(&grumble.Command{
+		Name: "getset",
+		Help: "get curvalue set newvalue",
+		Run:  stringGetSet,
+		Args: func(a *grumble.Args) {
+			a.String("key", "key", grumble.Default(""))
+			a.String("value", "value", grumble.Default(""))
+		},
+	})
+
+	app.AddCommand(&grumble.Command{
+		Name: "exist",
+		Help: "exist key",
+		Run:  stringExists,
+		Args: func(a *grumble.Args) {
+			a.String("key", "key", grumble.Default(""))
+		},
+	})
+
+	app.AddCommand(&grumble.Command{
+		Name: "MGet",
+		Help: "Gets the value of multiple keys simultaneously. MGet key1 key2 keyN",
+		Run:  stringMGet,
+		Args: func(a *grumble.Args) {
+			a.StringList("key", "key", grumble.Default(""))
+		},
+	})
+
+	app.AddCommand(&grumble.Command{
+		Name: "MSet",
+		Help: "Set multiple key pairs at the same time. MSet key1 value1 key2 value2 .. keyN valueN",
+		Run:  stringMSet,
+		Args: func(a *grumble.Args) {
+			a.StringList("keyvalue", "key value", grumble.Default(""))
+		},
+	})
 }
