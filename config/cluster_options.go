c8f7de88617c0ea80187c81da3bf4e2427210ea8
Saeid Aghapour <saeid.aghapour@gmail.com>
2023-07-12 18:25:28 +0330

    Rewrite the SnapshotStore component of raft (#178)

--- config/cluster_options.go
+++ config/cluster_options.go
@@ -3,11 +3,13 @@ package config
 import "time"
 
 type Config struct {
-	ReplicationFactor  int
-	ShardingStrategy   string
-	SchedulingStrategy string
-	LogDataStorage     string
-	LogDataStoragePath string
-	LogDataStorageSize int64
-	HeartbeatInterval  time.Duration
+	ReplicationFactor   int
+	ShardingStrategy    string
+	SchedulingStrategy  string
+	LogDataStorage      string
+	LogDataStoragePath  string
+	SnapshotStorage     string
+	SnapshotStoragePath string
+	LogDataStorageSize  int64
+	HeartbeatInterval   time.Duration
 }
