88a35ef033b8ab7c5e58ebcd4401989f1d3a7a73
sjcsjc123 <1401189096@qq.com>
2023-07-29 01:57:48 +0800

    refactor component for store and region

--- cluster/meta/meta.go
+++ cluster/meta/meta.go
@@ -1,6 +1,7 @@
 package meta
 
 import (
+	"fmt"
 	"github.com/ByteStorage/FlyDB/cluster/region"
 	"github.com/ByteStorage/FlyDB/cluster/store"
 	"github.com/ByteStorage/FlyDB/config"
@@ -24,6 +25,8 @@ type MetadataManager interface {
 	GetRegionByID(id uint64) (*region.Region, error)
 	// GetStoreByID gets a store by id.
 	GetStoreByID(id uint64) (*store.Store, error)
+	// ApplyConfig applies a new config to the cluster.
+	ApplyConfig(config *config.Config) error
 }
 
 // meta stores the metadata of the cluster.
@@ -39,38 +42,78 @@ type meta struct {
 	mu            sync.RWMutex              // mutex, to protect the metadata.
 	scheduler     *Scheduler                // scheduler, to schedule the cluster.
 	raft          *raft.Raft                // raft, to store the raft group.
+	addr          string                    // address, to store the address of the meta node.
 }
 
+// GetStore gets a store by address.
 func (m *meta) GetStore(addr string) (*store.Store, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
+// AddStore adds a new store to the cluster.
 func (m *meta) AddStore(addr string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
+// RemoveStore removes a store from the cluster.
 func (m *meta) RemoveStore(addr string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
+// GetAllStores gets all stores in the cluster.
 func (m *meta) GetAllStores() []*store.Store {
 	//TODO implement me
 	panic("implement me")
 }
 
+// GetRegionByID gets a region by id.
 func (m *meta) GetRegionByID(id uint64) (*region.Region, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
+// GetStoreByID gets a store by id.
 func (m *meta) GetStoreByID(id uint64) (*store.Store, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
+// ApplyConfig applies a new config to the cluster.
+func (m *meta) ApplyConfig(config *config.Config) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m != nil {
+		err := m.stop()
+		if err != nil {
+			// if err = ErrNotStarted, it means the meta node has not started yet.
+		}
+		err = m.start()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *meta) start() error {
+	for _, metaNode := range m.clusterConfig.MetaNodes {
+		// ssh to the meta node
+		// start the meta node
+		fmt.Println(metaNode)
+		panic("implement me")
+	}
+	return nil
+}
+
+func (m *meta) stop() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+// NewMeta creates a new meta.
 func NewMeta(conf config.Config) MetadataManager {
 	return &meta{
 		clusterConfig: &conf,
