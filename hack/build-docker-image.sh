b408027b57819a3e23c001da01d5e326429d776a
sjcsjc123 <1401189096@qq.com>
2024-01-15 15:25:21 +0800

    init Makefile and hack

--- hack/build-docker-image.sh
+++ hack/build-docker-image.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-DOCKER_USERNAME="$DOCKER_USERNAME"
-IMAGE_NAME="$IMAGE_NAME"
-TAG="$TAG"
+# 提示用户输入 Docker Hub 相关信息
+# shellcheck disable=SC2162
+read -p "Please enter your Docker Hub username: " DOCKER_USERNAME
+# shellcheck disable=SC2162
+read -p "Please enter image name: " IMAGE_NAME
+# shellcheck disable=SC2162
+read -p "Please enter image tag: " TAG
 
+# check docker command
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Docker is not installed"
+    exit 1
+fi
+
+# check go command
+if ! [ -x "$(command -v go)" ]; then
+    echo "Go is not installed"
+    exit 1
+fi
+
+# check environment variables
 if [ -z "$DOCKER_USERNAME" ] || [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
     echo "Please set DOCKER_USERNAME, IMAGE_NAME and TAG environment variables"
     exit 1
 fi
 
-docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TAG .
+go build -o bin/flydb-server cmd/server/cli/flaydb-server.go
+
+# build docker image
+docker build -t DOCKER_USERNAME/IMAGE_NAME:TAG -f docker/Dockerfile .
 
 # docker login
 
-docker push $DOCKER_USERNAME/$IMAGE_NAME:$TAG
+# push docker image
+docker push DOCKER_USERNAME/IMAGE_NAME:TAG
 
 # docker rmi $DOCKER_USERNAME/$IMAGE_NAME:$TAG
