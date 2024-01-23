d479500fb1668b63b3850345c098a2777768b803
sjcsjc123 <1401189096@qq.com>
2024-01-23 18:16:24 +0800

    recover docker

--- hack/build-docker-image.sh
+++ hack/build-docker-image.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# 提示用户输入 Docker Hub 相关信息
-# shellcheck disable=SC2162
-read -p "Please enter your Docker Hub username: " DOCKER_USERNAME
-# shellcheck disable=SC2162
-read -p "Please enter image name: " IMAGE_NAME
-# shellcheck disable=SC2162
-read -p "Please enter image tag: " TAG
+DOCKER_USERNAME="${DOCKER_USERNAME:-bytestorage}"
+IMAGE_NAME="${IMAGE_NAME:-flydb}"
+TAG="${TAG:-latest}"
 
 # check docker command
 if ! [ -x "$(command -v docker)" ]; then
@@ -20,24 +16,16 @@ if ! [ -x "$(command -v go)" ]; then
     exit 1
 fi
 
-# check environment variables
-if [ -z "$DOCKER_USERNAME" ] || [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
+if [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
     echo "Please set DOCKER_USERNAME, IMAGE_NAME and TAG environment variables"
     exit 1
 fi
 
 go build -o bin/flydb-server cmd/server/cli/flydb-server.go
 
-echo "DOCKER_USERNAME: $DOCKER_USERNAME"
 echo "IMAGE_NAME: $IMAGE_NAME"
 echo "TAG: $TAG"
 
 # build docker image
 docker build -t "$DOCKER_USERNAME/$IMAGE_NAME:$TAG" -f docker/Dockerfile .
 
-# docker login
-
-# push docker image
-docker push "$DOCKER_USERNAME/$IMAGE_NAME:$TAG"
-
-# docker rmi $DOCKER_USERNAME/$IMAGE_NAME:$TAG
